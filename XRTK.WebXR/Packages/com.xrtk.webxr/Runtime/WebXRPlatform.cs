529f289a617a04a5f091f14c174c8a3030445f46
Stephen Hodgson <hodgson.designs@gmail.com>
2021-05-17 11:05:59 -0400

    updated webxr platform definition

--- XRTK.WebXR/Packages/com.xrtk.webxr/Runtime/WebXRPlatform.cs
+++ XRTK.WebXR/Packages/com.xrtk.webxr/Runtime/WebXRPlatform.cs
@@ -1,28 +1,17 @@
 // Copyright (c) XRTK. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using XRTK.Interfaces;
+
 namespace XRTK.Definitions.Platforms
 {
     /// <summary>
     /// Used by the XRTK to signal that the feature is available on the WebXR platform.
     /// </summary>
     [System.Runtime.InteropServices.Guid("014aed5a-a0d8-4db3-a3c0-0b33ff7c065b")]
-    public class WebXRPlatform : BasePlatform
+    public class WebXRPlatform : WebGlPlatform
     {
         /// <inheritdoc />
-        public override bool IsAvailable => !UnityEngine.Application.isEditor;
-
-        /// <inheritdoc />
-        public override bool IsBuildTargetAvailable
-        {
-            get
-            {
-#if UNITY_EDITOR
-                return (UnityEditor.EditorUserBuildSettings.activeBuildTarget == UnityEditor.BuildTarget.WebGL);
-#else
-                return false;
-#endif
-            }
-        }
+        public override IMixedRealityPlatform[] PlatformOverrides => new IMixedRealityPlatform[] { new WebGlPlatform() };
     }
 }
