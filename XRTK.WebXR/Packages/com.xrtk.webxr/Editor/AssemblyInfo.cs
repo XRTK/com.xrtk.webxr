81058eaad511e5768093c338da0434f17b6569fc
Stephen Hodgson <hodgson.designs@gmail.com>
2021-05-17 10:22:28 -0400

    Added assembly infos
    updated script icons

---
+++ XRTK.WebXR/Packages/com.xrtk.webxr/Editor/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+// Copyright (c) XRTK. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#define XRTK_WEB_XR_EDITOR
+
+using System.Reflection;
+
+[assembly: AssemblyVersion("0.2.0")]
+[assembly: AssemblyTitle("com.xrtk.webxr.editor")]
+[assembly: AssemblyCompany("XRTK")]
+[assembly: AssemblyCopyright("Copyright (c) XRTK. All rights reserved.")]
