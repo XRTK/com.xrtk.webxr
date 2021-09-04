ffed4a72b2d90e6d0f2f66f80fb6442c920afc93
Stephen Hodgson <hodgson.designs@gmail.com>
2021-09-04 18:35:20 -0400

    updated project to 2019.4.30f1
    updated xrtk.core pacakge

--- XRTK.WebXR/Packages/com.xrtk.webxr/Runtime/AssemblyInfo.cs
+++ XRTK.WebXR/Packages/com.xrtk.webxr/Runtime/AssemblyInfo.cs
@@ -5,7 +5,7 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.2.0")]
+[assembly: AssemblyVersion("0.3.0")]
 [assembly: AssemblyTitle("com.xrtk.webxr")]
 [assembly: AssemblyCompany("XRTK")]
 [assembly: AssemblyCopyright("Copyright (c) XRTK. All rights reserved.")]
